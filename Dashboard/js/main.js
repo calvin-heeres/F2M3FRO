d846dffe449440ef591b5777ffc19a80098a6401
calvin-heeres <34248@ma-web.nl>
2022-03-21 09:26:18 +0100

    update

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -1,9 +1,10 @@
+/* Deze variabelen daar kan elk functie bij, want ze zijn geblobaal gedeclareerd */
 const  startButton = document.getElementById("js--start");
 const stopButton = document.getElementById("js--stop");
 const resetButton = document.getElementById("js--reset");
 let seconds = 0;
 let minutes = 0;
-let running = false; /* standaard telt die nog niet */
+let running = false; / standaard telt die nog niet /
 
 const secondsTimer = document.getElementById("js--secondsTimer")
 const minuteTimer = document.getElementById("js--minuteTimer")
@@ -11,7 +12,7 @@ const minuteTimer = document.getElementById("js--minuteTimer")
 let timer;
 
 startButton.onclick = function(){
-    if(running === true){ /* als de stopwatch loopt mag je niks doen */
+    if(running === true){ /stopwatch al loopt dan mag je niks doen/
         return;
     } 
     running = true;
@@ -22,7 +23,7 @@ startButton.onclick = function(){
             minuteTimer.innerText = minutes;
             seconds = 0;
         }
-        secondsTimer.innerText = seconds; /* <-- Dit is in milliseconden 1000ms = 1 seconden */
+        secondsTimer.innerText = seconds; / <-- Dit is in milliseconden 1000ms = 1 seconden /
     },  100); 
 }
 
@@ -39,7 +40,7 @@ resetButton.onclick = function(){
     secondsTimer.innerHTML = minutes;
 }
 
-/* De slider begint hier */
+/* Hier begint de slider */
 const rangeValue = document.getElementById("js--rangeValue");
 const slider = document.getElementById("js--slider");
 const body = document.getElementById("js--body");
